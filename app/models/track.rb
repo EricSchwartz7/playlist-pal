11414a51a804e97bbd1b807bbd2ac2bc8d7362ab
Ric <ericschwartz7@gmail.com>
2023-03-13 09:43:40 -0400

    add httparty gem

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,7 +1,8 @@
 class Track < ApplicationRecord
 
-  def index
-    render json: { foo: 'bar' }
+  def self.all_tracks
+    response = HTTParty.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+    response.body if response.code == 200
   end
 
 end
