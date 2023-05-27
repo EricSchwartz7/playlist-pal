14b379d83c98189bd56e126522b2ee357f8b4693
Ric <ericschwartz7@gmail.com>
2023-05-27 09:12:10 -0400

    start front end. add example payload

---
+++ playlist-pal/pages/tracks.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+export default function Tracks() {
+  return (
+    <>
+      <h1>All Tracks</h1>
+      <h2>
+        <Link href="/">Back to home</Link>
+      </h2>
+    </>
+  )
+}
